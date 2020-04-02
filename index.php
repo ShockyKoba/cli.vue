e433ed9a4c6ff6bd290a6fcef169b29594ee0d84
Rhevkan <ebel.bengoechea@gmail.com>
2020-04-02 11:10:14 -0600

    Vue Test

--- index.php
+++ index.php
@@ -15,25 +15,102 @@
 </head>
 <body>
     <div id="app">
+        <div class="wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1>{{title}}</h1>
 
-        <h1>{{title}}</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti necessitatibus doloribus ullam illo eius reiciendis excepturi unde nostrum, iure aliquid vero officiis esse aut perferendis optio sunt. Dolorum, quas hic?Consectetur at placeat eum fugiat sapiente totam saepe aperiam? Suscipit ipsam laboriosam, quo officiis, voluptatem minus eum eligendi aperiam vero optio dolorem quasi necessitatibus autem ex dolores iste. Quasi, aut.
+                        </p>
 
-        <input type="text" v-model="newfruit_name">
-        <input type="number" v-model="newfruit_quantity">
-        <input type="checkbox" v-model="newfruit_onsale">
+                        <table class="table mt-5 mb-0">
+                            <thead>
+                                <tr class="thead-dark">
+                                    <th>#</th>
+                                    <th>Fruit</th>
+                                    <th>Brand</th>
+                                    <th>Quantity</th>
+                                    <th>On Sale</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(fruit, index) of fruits">
+                                    <td>{{index}}</td>
+                                    <td>{{fruit.name}}</td>
+                                    <td>{{fruit.branch }}</td>
+                                    <td>{{fruit.quantity}}</td>
+                                    <td v-if="fruit.onsale == true">Yes</td>
+                                    <td v-else>No</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        
+                    </div>
+                    <div class="col-12 mt-5">
+                        <hr>
+                        <h2 class="mt-5">New Fruit : </h2>
+                        <form id="add-fruit" @submit="FormCheck" method="POST">
+                            <div class="row">
+                                <div class="col-12" v-if="newfruit_error.length">
+                                    <b>Porfavor corrija los siguientes errores: </b>
+                                    <ul>
+                                        <li v-for="err of newfruit_error">
+                                            {{err}}
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-12 col-sm-3">
+                                    <label class="mb-3" for="fruit-name">
+                                        Fruit: 
+                                    </label>
+                                </div>
+                                <div class="col-12 col-md-9">
+                                    <input type="text" id="fruit-name" v-model="newfruit_name">
+                                </div>
 
-        <button @click="addfruit">Add Fruit</button>
+                                <div class="col-12 col-sm-3">
+                                    <label class="mb-3" for="fruit-branch">
+                                        Branch:
+                                    </label>
+                                </div>
+                                <div class="col-12 col-md-9">
+                                    <input type="text" id="fruit-branch" v-model="newfruit_branch">
+                                </div>
 
-        <ul>
-            <li v-for="fruit of fruits">
-                {{fruit.name}} -
-                {{fruit.quantity}} 
-                <span v-if="fruit.onsale == false">
-                    - It's not for sale!
-                </span>
-            </li>
-        </ul>
+                                <div class="col-12 col-sm-3">
+                                    <label class="mb-3" for="fruit-quantity">
+                                        Quantity:
+                                    </label>
+                                </div>
+                                <div class="col-12 col-md-9">
+                                    <input type="text" id="fruit-quantity" v-model="newfruit_quantity">
+                                </div>
 
+                                <div class="col-12 col-sm-3">
+                                    <label class="mb-3" for="fruit-onsale">
+                                        On Sale: 
+                                    </label>
+                                </div>
+                                <div class="col-12 col-md-9">
+                                    <input type="checkbox" id="fruit-onsale" v-model="newfruit_onsale">
+                                </div>
+
+                                <div class="col-12 col-sm-3">
+                                    <label class="mb-3">
+                                        <input type="submit" class="btn btn-primary" value="Add Fruit">
+                                    </label>
+                                </div>
+                            </div>
+                        </form>
+
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
--- js/master.js
+++ js/master.js
@@ -2,10 +2,16 @@ const app = new Vue({
     el: '#app',
     data: {
         title: 'Welcome!',
-        fruits: [{name: 'apple', quantity: 12, onsale: true}, {name: 'peach', quantity: 24, onsale: false}, {name: 'lemon', quantity: 60, onsale: true}],
+        fruits: [
+            {name: 'apple', branch: 'Hello', quantity: 12, onsale: true}, 
+            {name: 'peach', branch: 'Kitty', quantity: 24, onsale: false}, 
+            {name: 'lemon', branch: 'Lex', quantity: 60, onsale: true}
+        ],
         newfruit_name: '',
+        newfruit_branch: '',
         newfruit_quantity: 0,
-        newfruit_onsale: false
+        newfruit_onsale: false,
+        newfruit_error: ''
     },
     methods: {
         addfruit () {
@@ -19,3 +25,5 @@ const app = new Vue({
     }
 
 }); 
+
+
